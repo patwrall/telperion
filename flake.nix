b84d8a05a664f371ab810f6fc00f6641e873cf57
Patrick Rall <patrickwrall@gmail.com>
2025-07-12 22:59:17 -0500

    chore: further project setup

--- flake.nix
+++ flake.nix
@@ -1,15 +1,38 @@
 {
-  description = "A very basic flake";
+  description = "My NixOS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nvf.url = "github:notashelf/nvf";
+    stylix.url = "github:danth/stylix/release-25.05";
+    nix-flatpak.url = "github:gmodena/nix-flatpak?ref=latest";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    systems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
   };
 }
