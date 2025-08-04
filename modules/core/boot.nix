6d15487bbb0cc1787e542d59c5072ec374e7f687
Patrick Rall <patrickwrall@gmail.com>
2025-08-03 19:54:29 -0500

    fix(boot): v4l2l kernerlPackages instead of kernelModules

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -1,11 +1,13 @@
 { pkgs
 , config
 , ...
-}: {
+}:
+{
   boot = {
     kernelPackages = pkgs.linuxPackages;
     kernelModules = [ "v4l2loopback" ];
-    extraModulePackages = [ config.boot.kernelModules.v4l2loopback ];
+    extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+    kernel.sysctl = { "vm.max_map_count" = 2147483642; };
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
     binfmt.registrations.appimage = {
@@ -18,3 +20,4 @@
     };
   };
 }
+
