d3e8f3a02914b15e5862b5db09453721b5b793e9
Patrick Rall <patrickwrall@gmail.com>
2025-08-03 19:25:22 -0500

    fix: missing import from system.nix inherit

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -2,7 +2,7 @@
 , ...
 }:
 let
-  inherit (../../hosts/${host}/variables.nix) consoleKeyMap;
+  inherit (import ../../hosts/${host}/variables.nix) consoleKeyMap;
 in
 {
   nix = {
@@ -24,6 +24,21 @@ in
       require-sigs = true;
     };
   };
+
+  time.timeZone = "America/Chicago";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
   console.keymap = consoleKeyMap;
   system.stateVersion = "25.05";
 }
