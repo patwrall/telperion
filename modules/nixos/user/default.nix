041b31643d046549a3c43414aacc0219a14dcb40
Patrick Rall <patrickwrall@gmail.com>
2025-08-07 00:02:08 -0500

    feat(nixos/user): user module

---
+++ modules/nixos/user/default.nix
@@ -0,0 +1,51 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
+let
+  inherit (lib) types;
+  inherit (lib.telperion) mkOpt;
+
+  cfg = config.telperion.user;
+in
+{
+  options.telperion.user = with types; {
+    email = mkOpt str "patwrall@gmail.com" "The email of the user.";
+    extraGroups = mkOpt (listOf str) [ ] "Groups for the user to be assigned.";
+    extraOptions = mkOpt attrs { } "Extra options passed to <option>users.users.<name></option>.";
+    fullName = mkOpt str "Patrick Rall" "The full name of the user.";
+    initialPassword =
+      mkOpt str "password"
+        "The initial password to use when the user is first created.";
+    name = mkOpt str "pat" "The name to use for the user account.";
+  };
+
+  config = {
+    users.users.${cfg.name} = {
+      inherit (cfg) name initialPassword;
+
+      extraGroups = [
+        "wheel"
+        "systemd-journal"
+        "mpd"
+        "audio"
+        "video"
+        "input"
+        "plugdev"
+        "lp"
+        "tss"
+        "power"
+        "nix"
+      ]
+      ++ cfg.extraGroups;
+
+      group = "users";
+      home = "/home/${cfg.name}";
+      isNormalUser = true;
+      shell = pkgs.zsh;
+      uid = 1000;
+    }
+    // cfg.extraOptions;
+  };
+}
