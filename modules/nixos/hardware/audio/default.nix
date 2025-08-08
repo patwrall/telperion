4dccfffc407690ad822d5ca2f667483b5bede8f7
Patrick Rall <patrickwrall@gmail.com>
2025-08-08 13:02:20 -0500

    feat(audo): audio

---
+++ modules/nixos/hardware/audio/default.nix
@@ -0,0 +1,69 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
+let
+  inherit (lib) types mkIf mkForce;
+  inherit (lib.telperion) mkOpt;
+
+  cfg = config.telperion.hardware.audio;
+in
+{
+  options.telperion.hardware.audio = with types; {
+    enable = lib.mkEnableOption "audio support";
+    alsa-monitor = mkOpt attrs { } "Alsa configuration.";
+    extra-packages = mkOpt (listOf package) [
+      pkgs.qjackctl
+      pkgs.easyeffects
+    ] "Additional packages to install.";
+    modules = mkOpt (listOf attrs) [ ] "Audio modules to pass to Pipewire as `context.modules`.";
+    nodes = mkOpt (listOf attrs) [ ] "Audio nodes to pass to Pipewire as `context.objects`.";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages =
+      with pkgs;
+      [
+        pulsemixer
+        pavucontrol
+        helvum
+      ]
+      ++ cfg.extra-packages;
+
+    telperion = {
+      user.extraGroups = [ "audio" ];
+    };
+
+    # Disable audio power saving to prevent crackling
+    boot.extraModprobeConfig = ''
+      options snd_hda_intel power_save=0
+    '';
+
+    security.rtkit.enable = true;
+
+    services = {
+      pipewire = {
+        enable = true;
+        alsa.enable = true;
+        audio.enable = true;
+        jack.enable = true;
+        pulse.enable = true;
+        wireplumber.enable = true;
+        extraConfig.pipewire."99-low-latency" = {
+          context.properties = {
+            default = {
+              clock = {
+                rate = 48000;
+                quantum = 512;
+                min-quantum = 256;
+                max-quantum = 8192;
+              };
+            };
+          };
+        };
+      };
+      pulseaudio.enable = mkForce false;
+    };
+  };
+}
