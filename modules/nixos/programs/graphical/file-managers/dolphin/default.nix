04848bb5e2deb9be277ad52230da60f14462dafc
Patrick Rall <patrickwrall@gmail.com>
2025-08-12 13:14:40 -0500

    feat(nixos/graphical/file-managers): added dolphin

---
+++ modules/nixos/programs/graphical/file-managers/dolphin/default.nix
@@ -0,0 +1,22 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
+let
+  inherit (lib) mkIf;
+
+  cfg = config.telperion.programs.graphical.file-managers.dolphin;
+in
+{
+  options.telperion.programs.graphical.file-managers.dolphin = {
+    enable = lib.mkEnableOption "Dolphin";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ dolphin-emu ];
+
+    # Enable GameCube controller support.
+    services.udev.packages = [ pkgs.dolphinEmu ];
+  };
+}
