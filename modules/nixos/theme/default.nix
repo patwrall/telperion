04648f6fd7d555fdbb09f4d471f1050ef03b61ce
Patrick Rall <patrickwrall@gmail.com>
2025-08-12 21:31:34 -0500

    refactor(themes): moved cursor to global themes

---
+++ modules/nixos/theme/default.nix
@@ -0,0 +1,42 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
+let
+  cfg = config.telperion.theme;
+in
+{
+  options.telperion.theme = {
+    enable = lib.mkEnableOption "system-wide cursor theme";
+
+    cursor = {
+      name = lib.mkOption {
+        type = lib.types.str;
+        default = "Bibata-Modern-Classic";
+        description = "The name of the cursor theme to apply.";
+      };
+      package = lib.mkOption {
+        type = lib.types.package;
+        default = pkgs.bibata-cursors;
+        description = "The package providing the cursor theme.";
+      };
+      size = lib.mkOption {
+        type = lib.types.int;
+        default = 24;
+        description = "The size of the cursor.";
+      };
+    };
+  };
+
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ cfg.cursor.package ];
+
+    environment.sessionVariables = {
+      CURSOR_THEME = cfg.cursor.name;
+      XCURSOR_THEME = cfg.cursor.name;
+      XCURSOR_SIZE = toString cfg.cursor.size;
+    };
+  };
+}
