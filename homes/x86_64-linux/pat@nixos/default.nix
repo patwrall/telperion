505597123271c20fbe556180c149fc451ec25052
Patrick Rall <patrickwrall@gmail.com>
2025-08-16 20:16:54 -0500

    feat(home/system): xdg

--- homes/x86_64-linux/pat@nixos/default.nix
+++ homes/x86_64-linux/pat@nixos/default.nix
@@ -1,4 +1,20 @@
 { config
 , lib
 , ...
-}: { }
+}:
+let
+  inherit (lib) getExe;
+  inherit (lib.telperion) enabled;
+in
+{
+  telperion = {
+    user = {
+      enable = true;
+      name = "pat";
+    };
+
+    system = {
+      xdg = enabled;
+    };
+  };
+}
