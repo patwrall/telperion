d8e296d76461857fc7625066501ed6bbe8c2ef48
Patrick Rall <patrickwrall@gmail.com>
2025-08-22 21:56:15 -0500

    feat(homes/x86_64-linux/pat@laptop): pat@laptop module

--- homes/x86_64-linux/pat@laptop/default.nix
+++ homes/x86_64-linux/pat@laptop/default.nix
@@ -1,4 +1,35 @@
-{ config
-, lib
+{ lib
 , ...
-}: { }
+}:
+let
+  inherit (lib.telperion) enabled;
+in
+{
+  telperion = {
+    user = {
+      enable = true;
+      name = "pat";
+    };
+
+    programs = {
+      graphical = {
+        quickshell = {
+          caelestia = enabled;
+        };
+        wms = {
+          hyprland = enabled;
+        };
+      };
+    };
+
+    system = {
+      xdg = enabled;
+    };
+
+    suites = {
+      common = enabled;
+    };
+  };
+
+  home.stateVersion = "25.05";
+}
