03ebe81852f8246539b1a304ca42d0b5be0c2f4d
Patrick Rall <patrickwrall@gmail.com>
2025-08-22 20:26:39 -0500

    feat(nixos/security/keyring): keyring module

---
+++ modules/nixos/security/keyring/default.nix
@@ -0,0 +1,19 @@
+{ config
+, lib
+, ...
+}:
+let
+  inherit (lib) mkIf;
+
+  cfg = config.khanelinix.security.keyring;
+in
+{
+  options.khanelinix.security.keyring = {
+    enable = lib.mkEnableOption "gnome keyring";
+  };
+
+  config = mkIf cfg.enable {
+    # NOTE: Also enables services.gnome.gcr-ssh-agent apparently
+    services.gnome.gnome-keyring.enable = true;
+  };
+}
