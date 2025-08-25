6c1b9a4588ba2e5171e009600ace533f1a0d976f
Patrick Rall <patrickwrall@gmail.com>
2025-08-24 19:31:15 -0500

    feat(lib/system): import caelestia shell hm module

--- lib/system/mk-home.nix
+++ lib/system/mk-home.nix
@@ -35,6 +35,7 @@ inputs.home-manager.lib.homeManagerConfiguration {
     inputs.nix-index-database.homeModules.nix-index
     inputs.sops-nix.homeManagerModules.sops
     inputs.opnix.homeManagerModules.default
+    inputs.caelestia-shell.homeManagerModules.default
 
     # Import all home modules recursively
   ]
