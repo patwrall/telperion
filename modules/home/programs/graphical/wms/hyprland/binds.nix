cffa8a5e43892554c10b1b401b899df0a5a3b838
Patrick Rall <patrickwrall@gmail.com>
2025-09-09 20:46:34 -0500

    fix(home/programs/graphical): ok revert to uwsm for debug

--- modules/home/programs/graphical/wms/hyprland/binds.nix
+++ modules/home/programs/graphical/wms/hyprland/binds.nix
@@ -1,5 +1,6 @@
 { config
 , lib
+, osConfig ? { }
 , ...
 }:
 let
@@ -7,7 +8,8 @@ let
 
   cfg = config.telperion.programs.graphical.wms.hyprland;
 
-  mkStartCommand = cmd: "app2unit -- ${cmd}";
+  mkStartCommand = cmd:
+    if (osConfig.programs.uwsm.enable or false) then "uwsm app -- ${cmd}" else "app2unit -- ${cmd}";
 
   mkExecBind = bind:
     let
