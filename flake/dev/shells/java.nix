bb4baa3536c7eb01dfc1787a050ebcf1539cb741
Patrick Rall <patrickwrall@gmail.com>
2025-09-18 22:31:31 -0400

    fix(flake/dev/shells): help linker out a bit
    need this for some school labs

--- flake/dev/shells/java.nix
+++ flake/dev/shells/java.nix
@@ -8,12 +8,20 @@ mkShell {
     jdk8
     jdk11
     jdk17
+    openjfx17
     temurin-jre-bin-17
     maven
     gradle
+    glib
   ];
 
   shellHook = ''
+
+    export PATH="$PATH:${pkgs.maven}/bin"
+    export JAVA_HOME="${pkgs.jdk17}/lib/openjdk"
+    export PATH="$PATH:$JAVA_HOME/bin"
+    export LD_LIBRARY_PATH="${pkgs.libGL}/lib:${pkgs.gtk3}/lib:${pkgs.glib.out}/lib:${pkgs.xorg.libXtst}/lib:$LD_LIBRARY_PATH"   
+    export JAVAFX_PATH="${pkgs.openjfx17}/lib"
 
     echo 🔨 Java DevShell
 
